0825191b25bb44f8d19ef7e238a283006dab61fe
sarafasra <sarafasraoui75@gmail.com>
2026-05-12 12:42:10 +0100

    ajouter quelque modification sur creation des class

--- src/Entities/user.php
+++ src/Entities/user.php
@@ -3,16 +3,19 @@ class User {
     private int $id ;
     private string $name;
     private string $email;
+    private string $password;
 
-    public function __construct($id,$name,$email){
+    public function __construct($id,$name,$email,$password){
     
         $this->id = $id;
         $this->name = $name;
         $this->email = $email;
+        $this->password = password;
     }
         public function getId(){ return $this->id; }
         public function getName(){ return $this->name;}
         public function getEmail(){ return $this->email;}
+        public function getPssword(){ return $this->password;}
 
 }
 
