8c038141fcdce23cef0a51aeffa302007fc102e6
sarafasra <sarafasraoui75@gmail.com>
2026-05-12 12:52:40 +0100

    ajouter la méthode create pour insérer un utilisateur avec PDO

--- src/Repositories/questionRepo.php
+++ src/Repositories/questionRepo.php
@@ -5,6 +5,16 @@ class UserRepository{
     public function __construct(PDO $pdo){
         $this->pdo = $pdo;
     }
+    public function create(User $user){
+        $sql = "INSERT INTO users(name,email,password)
+        VALUSE(?,?,?)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([
+            $user->getName(),
+            $user->getEmail(),
+            Password_hash($user->getPassword(),PASSWORD_DEFAULT),
+        ]);
+    }
 } 
 
 
