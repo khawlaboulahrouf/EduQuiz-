45fc82d6d9d59362fad539026f20a39d16aa4cc0
sarafasra <sarafasraoui75@gmail.com>
2026-05-12 12:30:42 +0100

    ajouter les getters

--- src/Entities/user.php
+++ src/Entities/user.php
@@ -10,7 +10,9 @@ class User {
         $this->name = $name;
         $this->email = $email;
     }
-
+        public function getId(){ return $this->id; }
+        public function getName(){ return $this->name;}
+        public function getEmail(){ return $this->email;}
 
 }
 
